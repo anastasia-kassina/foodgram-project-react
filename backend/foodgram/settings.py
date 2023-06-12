5a44721eb2806088895ef15a7fec6b4b454bcec2
Анастасия Кассина <kassnaanastacia@yandex.ru>
2023-06-13 02:04:47 +0700

    done

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = str(os.getenv('SECRET_KEY', default='12345'))
 
 DEBUG = False
 
