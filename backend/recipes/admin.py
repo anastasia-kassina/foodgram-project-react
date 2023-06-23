c82b6648e498b840a089cd394a77b462322a2977
Анастасия Кассина <kassnaanastacia@yandex.ru>
2023-06-24 00:06:42 +0700

    fix

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -19,19 +19,24 @@ class RecipeAdmin(admin.ModelAdmin):
     def added_in_favorites(self, obj):
         return obj.favorites.count()
 
+
 class RecipeIngredientsInLine(admin.TabularInline):
     model = Recipe.ingredient.through
     extra = 1
 
+
 class RecipeTagsInLine(admin.TabularInline):
     model = Recipe.tags.through
     extra = 1
+
+
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit',)
     search_fields = ('name',)
     empty_value_display = '-пусто-'
     inlines = (RecipeIngredientsInLine, RecipeTagsInLine)
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
