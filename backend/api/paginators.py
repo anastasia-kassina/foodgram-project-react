936d19824f1d5a33e544f0603e84b31d5a0cfa79
Анастасия Кассина <kassnaanastacia@yandex.ru>
2023-06-24 23:30:24 +0700

    fix

--- backend/api/paginators.py
+++ backend/api/paginators.py
@@ -2,6 +2,6 @@ from django.conf import settings
 from rest_framework.pagination import PageNumberPagination
 
 
-LimitPageNumberPagination(PageNumberPagination):
+class LimitPageNumberPagination(PageNumberPagination):
     page_size = settings.PAGE_SIZE
     page_size_query_param = 'limit'
